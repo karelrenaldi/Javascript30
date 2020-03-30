2b2139eb21c8c3ad2113606cbaddad3e189d1b95
Karel Renaldi <karelrenaldi8@gmail.com>
2020-03-30 23:17:23 +0700

    Finish Project 1 (Drum Kit)

---
+++ Project1-DrumKit/app.js
@@ -0,0 +1,17 @@
+const keys = document.querySelectorAll(".key");
+const run = currentKey => {
+    const keyAudio = document.querySelector(`audio[data-key = "${currentKey}"]`);
+    const keyAnimation = document.querySelector(`div[data-key= "${currentKey}"]`)
+    keyAnimation.classList.add("animation");
+    keyAnimation.addEventListener('transitionend', () => {
+        keyAnimation.classList.remove("animation");
+    },{once : true});
+    keyAudio.currentTime = 0; // Rewind audio
+    keyAudio.play();
+}
+// When user use keyboard
+window.addEventListener("keyup",(e)=>{
+    const currentKey = e.keyCode;
+    if(!document.querySelector(`audio[data-key = "${e.keyCode}"]`)) return
+    run(currentKey);
+})
